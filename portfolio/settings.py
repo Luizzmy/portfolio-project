2858590a56bb63e1375a02ca1563322651631ddc
Luizzmy <luisg.valenzuelam@gmail.com>
2021-01-03 12:15:53 +0200

    env fixed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+env = environ.Env()
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
