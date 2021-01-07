4a1c1f43dba4762261b59dc7138b7f959a1e1b21
Luizzmy <luisg.valenzuelam@gmail.com>
2021-01-07 15:21:43 +0200

    details url

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,7 @@ class Job(models.Model):
     # Will have an image and summary
     image=models.ImageField(upload_to='images/')
     summary=models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.summary
+
--- jobs/views.py
+++ jobs/views.py
@@ -6,5 +6,9 @@ def home(request):
     jobs=Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
 
+def detail(request, job_id):
+    print(job_id)
+    return render(request, 'jobs/home.html', )
+
 def gabriel(request):
     return render(request, 'jobs/gabriel.html')
